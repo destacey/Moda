3bf9c87ab249d1e5c21d98ffcc16b73182982972
Dannie Stacey <dstacey@outlook.com>
2022-09-04 11:25:00 -0500

    Added a domain common project with base entity models.

---
+++ src/Domain/Moda.Domain.Common/Data/BaseAuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Moda.Domain.Common.Data;
+
+public abstract class BaseAuditableEntity : BaseEntity
+{
+    public DateTime Created { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public DateTime? LastModified { get; set; }
+
+    public string? LastModifiedBy { get; set; }
+}
