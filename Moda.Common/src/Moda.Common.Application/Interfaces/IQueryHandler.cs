bba41d7525bc23df0ca4e066a700619b0a4d5732
Dannie Stacey <dstacey@outlook.com>
2022-10-02 12:56:35 -0500

    Adding additional interfaces to ensure a result is always returned for constency.

---
+++ Moda.Common/src/Moda.Common.Application/Interfaces/IQueryHandler.cs
@@ -0,0 +1,7 @@
+﻿using CSharpFunctionalExtensions;
+using MediatR;
+
+namespace Moda.Common.Application.Interfaces;
+public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>> where TQuery : IQuery<TResponse>
+{
+}
