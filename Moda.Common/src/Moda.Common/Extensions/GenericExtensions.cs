f050d8273718bdb0c5c6906192f1aba11ec20fcd
Dannie Stacey <dstacey@outlook.com>
2022-11-12 13:07:17 -0600

    Added a generic extension and tests to flatten a hierarchy.

---
+++ Moda.Common/src/Moda.Common/Extensions/GenericExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Moda.Common.Extensions;
+
+public static class GenericExtensions
+{
+    public static IEnumerable<T> FlattenHierarchy<T>(this T root, Func<T, IEnumerable<T>> branchSelector)
+    {
+        ArgumentNullException.ThrowIfNull(branchSelector);
+
+        Stack<T> stack = new ();
+        stack.Push(root);
+        
+        while (stack.Count > 0)
+        {
+            var current = stack.Pop();
+            yield return current;
+
+            if (current is null)
+                continue;
+
+            foreach (var child in branchSelector(current) ?? Enumerable.Empty<T>())
+            {
+                stack.Push(child);
+            }
+        }
+    }
+}
