38e8e035a55960f7062b53b2a20ca538649a5463
Dannie Stacey <dstacey@outlook.com>
2022-11-26 08:44:59 -0600

    Added the api for WorkTypes.

---
+++ Moda.Web/src/Moda.Web.Api/Models/Work/WorkTypes/CreateWorkTypeRequest.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using Moda.Work.Application.WorkTypes.Commands;
+
+namespace Moda.Web.Api.Models.Work.WorkTypes;
+
+public sealed record CreateWorkTypeRequest
+{
+    /// <summary>The name of the work type.  The name cannot be changed.</summary>
+    /// <value>The name.</value>
+    public required string Name { get; set; }
+
+    /// <summary>The description of the work type.</summary>
+    /// <value>The description.</value>
+    public string? Description { get; set; }
+
+    public CreateWorkTypeCommand ToCreateWorkTypeCommand()
+    {
+        return new CreateWorkTypeCommand(Name, Description);
+    }
+}
+
+public sealed class CreateWorkTypeRequestValidator : CustomValidator<CreateWorkTypeRequest>
+{
+    public CreateWorkTypeRequestValidator()
+    {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(c => c.Name)
+            .NotEmpty()
+            .MaximumLength(256);
+
+        RuleFor(c => c.Description)
+            .MaximumLength(1024);
+    }
+}
