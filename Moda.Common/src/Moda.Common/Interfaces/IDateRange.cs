f2a07163a17effbd1c982c71238edc41d7b3f992
Dannie Stacey <dstacey@outlook.com>
2023-03-26 09:55:14 -0500

    Added a LocalDateRange value object.

---
+++ Moda.Common/src/Moda.Common/Interfaces/IDateRange.cs
@@ -0,0 +1,10 @@
+﻿namespace Moda.Common.Interfaces;
+public interface IDateRange<T>
+{
+    T Start { get; }
+    T End { get; }
+
+    bool Includes(T value);
+    bool Includes(IDateRange<T> range);
+    bool Overlaps(IDateRange<T> range);
+}
