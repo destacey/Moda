5e4929185cc87fd1b4d46769412cf950382b7cb4
Dannie Stacey <dstacey@outlook.com>
2023-04-15 09:35:12 -0500

    Palette is obsolete.  Updated the DarkTheme to use the new PaletteDark.

--- Moda.Web/src/Moda.Web.BlazorClient/Infrastructure/Theme/DarkTheme.cs
+++ Moda.Web/src/Moda.Web.BlazorClient/Infrastructure/Theme/DarkTheme.cs
@@ -6,30 +6,42 @@ public class DarkTheme : MudTheme
 {
     public DarkTheme()
     {
-        Palette = new Palette()
+        Palette = new PaletteDark()
         {
-            Primary = CustomColors.Dark.Primary,
-            Secondary = CustomColors.Dark.Secondary,
-            Success = CustomColors.Dark.Primary,
+            // PaletteDark Overrides
             Black = "#27272f",
+            Primary = CustomColors.Dark.Primary,
+            Info = "#3299ff",
+            Success = CustomColors.Dark.Primary,
+            Warning = "#ffa800",
+            Error = "#f64e62",
+            Dark = CustomColors.Dark.DrawerBackground,
+            TextPrimary = "rgba(255,255,255, 0.70)",
+            TextSecondary = "rgba(255,255,255, 0.50)",
+            TextDisabled = CustomColors.Dark.Disabled,
+            ActionDefault = "#adadb1",
+            ActionDisabled = "rgba(255,255,255, 0.26)",
+            ActionDisabledBackground = "rgba(255,255,255, 0.12)",
             Background = CustomColors.Dark.Background,
             BackgroundGrey = "#27272f",
             Surface = CustomColors.Dark.Surface,
             DrawerBackground = CustomColors.Dark.DrawerBackground,
             DrawerText = "rgba(255,255,255, 0.50)",
+            DrawerIcon = "rgba(255,255,255, 0.50)",
             AppbarBackground = CustomColors.Dark.AppbarBackground,
             AppbarText = "rgba(255,255,255, 0.70)",
-            TextPrimary = "rgba(255,255,255, 0.70)",
-            TextSecondary = "rgba(255,255,255, 0.50)",
-            ActionDefault = "#adadb1",
-            ActionDisabled = "rgba(255,255,255, 0.26)",
-            ActionDisabledBackground = "rgba(255,255,255, 0.12)",
-            DrawerIcon = "rgba(255,255,255, 0.50)",
-            TableLines = "#e0e0e036",
-            Dark = CustomColors.Dark.DrawerBackground,
-            Divider = "#e0e0e036",
+            LinesDefault = "rgba(255,255,255, 0.12)",
+            LinesInputs = "rgba(255,255,255, 0.3)",
+            TableLines = "rgba(255,255,255, 0.12)",
+            TableStriped = "rgba(255,255,255, 0.06)",
+            Divider = "rgba(255,255,255, 0.12)",
+            DividerLight = "rgba(255,255,255, 0.06)",
+
+            // Palette Overrides
             OverlayDark = "hsl(0deg 0% 0% / 75%)",
-            TextDisabled = CustomColors.Dark.Disabled
+            Secondary = CustomColors.Dark.Secondary,
+            HoverOpacity = 0.40,
+            TableHover = "rgba(255,255,255, 0.25)"
         };
 
         LayoutProperties = new LayoutProperties()
