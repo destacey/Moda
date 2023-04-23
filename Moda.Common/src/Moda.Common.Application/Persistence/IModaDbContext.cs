c95d4e76ed65e41fcb2ecc85d5834e0e945b147d
Dannie Stacey <dstacey@outlook.com>
2023-04-23 13:34:05 -0500

    Adding the change tracker property to the IModaDbContext.

--- Moda.Common/src/Moda.Common.Application/Persistence/IModaDbContext.cs
+++ Moda.Common/src/Moda.Common.Application/Persistence/IModaDbContext.cs
@@ -3,6 +3,7 @@
 namespace Moda.Common.Application.Persistence;
 public interface IModaDbContext
 {
+    ChangeTracker ChangeTracker { get; }
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
     EntityEntry Entry(object entity);
