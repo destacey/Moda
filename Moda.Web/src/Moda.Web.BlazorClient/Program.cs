64eeb1374859867304f38a279740a1aedd5fb87e
Dannie Stacey <dstacey@outlook.com>
2023-05-04 06:00:42 -0500

    Disabling the blazor graph integration until we can figure out why it doesn't run in release mode.  Updated the layout.

--- Moda.Web/src/Moda.Web.BlazorClient/Program.cs
+++ Moda.Web/src/Moda.Web.BlazorClient/Program.cs
@@ -9,21 +9,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddClientServices(builder.Configuration);
-builder.Services.AddHttpClient("GraphAPI",
-        client => client.BaseAddress = new Uri("https://graph.microsoft.com"))
-    .AddHttpMessageHandler<GraphAPIAuthorizationMessageHandler>();
-
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-//builder.Services.AddMsalAuthentication(options =>
-//{
-//    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-
-//    options.ProviderOptions.DefaultAccessTokenScopes.Add("api://fdca5e6f-46a2-455c-b2f3-06a9a6877190/access_as_user");
-
-//    options.ProviderOptions.LoginMode = "redirect";
-//});
-
-//builder.Services.AddAuthorizationCore();
+//builder.Services.AddHttpClient("GraphAPI",
+//        client => client.BaseAddress = new Uri("https://graph.microsoft.com"))
+//    .AddHttpMessageHandler<GraphAPIAuthorizationMessageHandler>();
 
 await builder.Build().RunAsync();
