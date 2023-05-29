088de87933388ce4cd80cc88b7c8667cdafe059d
Dannie Stacey <dstacey@outlook.com>
2023-05-29 16:27:11 -0500

    Added an extension method to convert a LocalDate to an Instant.

---
+++ Moda.Common/tests/Moda.Common.Tests/Sut/Extensions/LocalDateExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using NodaTime;
+
+namespace Moda.Common.Tests.Sut.Extensions;
+public class LocalDateExtensionsTests
+{
+    [Fact]
+    public void ToInstant()
+    {
+        // Arrange
+        var localDate = new LocalDate(2023, 1, 1);
+        var expected = Instant.FromUtc(2023, 1, 1, 0, 0, 0);
+
+        // Act
+        var result = localDate.ToInstant();
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
