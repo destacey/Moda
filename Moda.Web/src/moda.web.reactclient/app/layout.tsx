7d09d6b7dac94b99382fb5e6ed2d538127817fa3
Dannie Stacey <dstacey@outlook.com>
2023-06-10 17:58:51 -0500

    Removing the themeprovider

--- Moda.Web/src/moda.web.reactclient/app/layout.tsx
+++ Moda.Web/src/moda.web.reactclient/app/layout.tsx
@@ -8,7 +8,6 @@ import { Breadcrumb, Layout, Menu, Button, ConfigProvider, theme, Switch, Space 
 import { LaptopOutlined, NotificationOutlined, UserOutlined, VideoCameraOutlined, UploadOutlined, MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons'
 import lightTheme from './config/theme/light-theme';
 import darkTheme from './config/theme/dark-theme';
-import ThemeProvider from './config/theme/theme-provider';
 
 const { Header, Sider, Content } = Layout;
 const inter = Inter({ subsets: ['latin'] });
@@ -55,42 +54,40 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ConfigProvider theme={{ token: currentTheme === 'light' ? lightTheme : darkTheme }}>
-          <ThemeProvider>
-            <Layout className="layout" style={{ minHeight: '100vh' }}>
-              <Header style={{ display: 'flex', alignItems: 'center', backgroundColor: currentTheme === 'dark' ? '#262a2c' : '#2196f3' }}>
-                <Space size={10} >
-                  <h2>Moda</h2>
-                  <Switch checked={currentTheme === 'dark'}
-                    onChange={() => setCurrentTheme(currentTheme === 'dark' ? 'light' : 'dark')}
-                    checkedChildren="Dark"
-                    unCheckedChildren="Light" />
-                </Space>
-              </Header>
-              <Layout>
-                <Sider width={200} collapsedWidth={50} collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-                  <Menu
-                    mode="inline"
-                    defaultSelectedKeys={['1']}
-                    defaultOpenKeys={['sub1']}
-                    style={{ height: '100%', borderRight: 0 }}
-                    items={items2}
-                  />
-                </Sider>
-                <Layout style={{ padding: '0 24px 24px' }}>
-                  <Breadcrumb separator='>' style={{ margin: '16px 0' }} items={[{ title: 'Home', href: '/' }]} />
-                  <Content
-                    style={{
-                      padding: 24,
-                      margin: 0,
-                      minHeight: 280,
-                    }}
-                  >
-                    {children}
-                  </Content>
-                </Layout>
+          <Layout className="layout" style={{ minHeight: '100vh' }}>
+            <Header style={{ display: 'flex', alignItems: 'center', backgroundColor: currentTheme === 'dark' ? '#262a2c' : '#2196f3' }}>
+              <Space size={10} >
+                <h2>Moda</h2>
+                <Switch checked={currentTheme === 'dark'}
+                  onChange={() => setCurrentTheme(currentTheme === 'dark' ? 'light' : 'dark')}
+                  checkedChildren="Dark"
+                  unCheckedChildren="Light" />
+              </Space>
+            </Header>
+            <Layout>
+              <Sider width={200} collapsedWidth={50} collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                <Menu
+                  mode="inline"
+                  defaultSelectedKeys={['1']}
+                  defaultOpenKeys={['sub1']}
+                  style={{ height: '100%', borderRight: 0 }}
+                  items={items2}
+                />
+              </Sider>
+              <Layout style={{ padding: '0 24px 24px' }}>
+                <Breadcrumb separator='>' style={{ margin: '16px 0' }} items={[{ title: 'Home', href: '/' }]} />
+                <Content
+                  style={{
+                    padding: 24,
+                    margin: 0,
+                    minHeight: 280,
+                  }}
+                >
+                  {children}
+                </Content>
               </Layout>
             </Layout>
-          </ThemeProvider>
+          </Layout>
         </ConfigProvider>
       </body>
     </html>
