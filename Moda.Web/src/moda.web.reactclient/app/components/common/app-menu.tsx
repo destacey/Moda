72e458132bd17cdb89086a5409200971c7c82bc2
Dannie Stacey <dstacey@outlook.com>
2023-06-12 23:25:22 -0500

    Updated the nav template.

--- Moda.Web/src/moda.web.reactclient/app/components/common/app-menu.tsx
+++ Moda.Web/src/moda.web.reactclient/app/components/common/app-menu.tsx
@@ -1,28 +1,57 @@
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from "@ant-design/icons";
+import { TeamOutlined, HomeOutlined, SettingOutlined, ProjectOutlined, ScheduleOutlined, ShopOutlined } from "@ant-design/icons";
 import { Menu, MenuProps } from "antd";
 import Sider from "antd/es/layout/Sider";
-import React from "react";
 import { useState } from "react";
 
-const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
-    (icon, index) => {
-        const key = String(index + 1);
+type MenuItem = Required<MenuProps>['items'][number];
 
-        return {
-            key: `sub${key}`,
-            icon: React.createElement(icon),
-            label: `subnav ${key}`,
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group',
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
 
-            children: new Array(4).fill(null).map((_, j) => {
-                const subKey = index * 4 + j + 1;
-                return {
-                    key: subKey,
-                    label: `option${subKey}`,
-                };
-            }),
-        };
-    },
-);
+const items: MenuProps['items'] = [
+    getItem('Home', 'home1', <HomeOutlined />),
+    getItem('Organization', 'org1', <TeamOutlined />, [
+        getItem('Teams', 'org1.1', null),
+        getItem('Employees', 'org1.2', null),
+    ]),
+    getItem('Planning', 'plan1', <ScheduleOutlined />, [
+        getItem('Program Increments', 'plan1.1', null),
+        getItem('Iterations', 'plan1.2', null),
+        getItem('Sprints', 'plan1.3', null),
+    ]),
+    getItem('Products', 'pdc1', <ShopOutlined />, [
+        getItem('Product Lines', 'pdc1.1', null),
+        getItem('Product Types', 'pdc1.2', null),
+        getItem('Productss', 'pdc1.3', null),
+        { type: 'divider' },
+        getItem('Releases', 'pdc1.4', null),
+        getItem('Roadmaps', 'pdc1.5', null),
+    ]),
+    getItem('Projects', 'ppm1', <ProjectOutlined />, [
+        getItem('Portfolios', 'ppm1.1', null),
+        getItem('Programs', 'ppm1.2', null),
+        getItem('Projects', 'ppm1.3', null),
+    ]),
+    { type: 'divider' },
+    getItem('Admin', 'adm1', <SettingOutlined />, [
+        getItem('Users', 'adm1.1', null),
+        getItem('Roles', 'adm1.2', null),
+        getItem('Background Jobs', 'adm1.3', null),
+    ]),
+];
 
 export default function AppMenu() {
     const [collapsed, setCollapsed] = useState(false);
@@ -33,7 +62,7 @@ export default function AppMenu() {
                 defaultSelectedKeys={['1']}
                 defaultOpenKeys={['sub1']}
                 style={{ height: '100%', borderRight: 0 }}
-                items={items2}
+                items={items}
             />
         </Sider>
     )
