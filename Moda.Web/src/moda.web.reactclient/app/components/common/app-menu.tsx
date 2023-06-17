507b61c65043e96f9f2afa6f773503ffd6805f0f
Dannie Stacey <dstacey@outlook.com>
2023-06-17 07:30:10 -0500

    Added empty pages for all of the current Blazor list endpoints.  Updated the app-menu with links to the new pages.

--- Moda.Web/src/moda.web.reactclient/app/components/common/app-menu.tsx
+++ Moda.Web/src/moda.web.reactclient/app/components/common/app-menu.tsx
@@ -1,6 +1,7 @@
-import { TeamOutlined, HomeOutlined, SettingOutlined, ProjectOutlined, ScheduleOutlined, ShopOutlined } from "@ant-design/icons";
+import { TeamOutlined, HomeOutlined, SettingOutlined, ProjectOutlined, ScheduleOutlined, ShopOutlined, DesktopOutlined } from "@ant-design/icons";
 import { Menu, MenuProps } from "antd";
 import Sider from "antd/es/layout/Sider";
+import Link from "next/link";
 import { useState } from "react";
 
 type MenuItem = Required<MenuProps>['items'][number];
@@ -22,34 +23,36 @@ function getItem(
 }
 
 const items: MenuProps['items'] = [
-    getItem('Home', 'home1', <HomeOutlined />),
-    getItem('Organization', 'org1', <TeamOutlined />, [
-        getItem('Teams', 'org1.1', null),
-        getItem('Employees', 'org1.2', null),
+    getItem(<Link href='/'>Home</Link>, 'home1', <HomeOutlined />),
+    getItem('Organizations', 'org1', <TeamOutlined />, [
+        getItem(<Link href='/organizations/teams'>Teams</Link>, 'org1.1', null),
+        getItem(<Link href='/organizations/employees'>Employees</Link>, 'org1.2', null),
     ]),
     getItem('Planning', 'plan1', <ScheduleOutlined />, [
-        getItem('Program Increments', 'plan1.1', null),
-        getItem('Iterations', 'plan1.2', null),
-        getItem('Sprints', 'plan1.3', null),
+        getItem(<Link href='/planning/program-increments'>Program Increments</Link>, 'plan1.1', null),
+        // getItem('Iterations', 'plan1.2', null),
+        // getItem('Sprints', 'plan1.3', null),
     ]),
-    getItem('Products', 'pdc1', <ShopOutlined />, [
-        getItem('Product Lines', 'pdc1.1', null),
-        getItem('Product Types', 'pdc1.2', null),
-        getItem('Productss', 'pdc1.3', null),
-        { type: 'divider' },
-        getItem('Releases', 'pdc1.4', null),
-        getItem('Roadmaps', 'pdc1.5', null),
-    ]),
-    getItem('Projects', 'ppm1', <ProjectOutlined />, [
-        getItem('Portfolios', 'ppm1.1', null),
-        getItem('Programs', 'ppm1.2', null),
-        getItem('Projects', 'ppm1.3', null),
-    ]),
+    // getItem('Products', 'pdc1', <DesktopOutlined />, [
+    //     getItem('Product Lines', 'pdc1.1', null),
+    //     getItem('Product Types', 'pdc1.2', null),
+    //     getItem('Products', 'pdc1.3', null),
+    //     { type: 'divider' },
+    //     getItem('Releases', 'pdc1.4', null),
+    //     getItem('Roadmaps', 'pdc1.5', null),
+    //     { type: 'divider' },
+    //     getItem('Requirement Management', 'pdc1.6', null),
+    // ]),
+    // getItem('Projects', 'ppm1', <ProjectOutlined />, [
+    //     getItem('Portfolios', 'ppm1.1', null),
+    //     getItem('Programs', 'ppm1.2', null),
+    //     getItem('Projects', 'ppm1.3', null),
+    // ]),
     { type: 'divider' },
-    getItem('Admin', 'adm1', <SettingOutlined />, [
-        getItem('Users', 'adm1.1', null),
-        getItem('Roles', 'adm1.2', null),
-        getItem('Background Jobs', 'adm1.3', null),
+    getItem('Settings', 'set1', <SettingOutlined />, [
+        getItem(<Link href='/settings/users'>Users</Link>, 'set1.1', null),
+        getItem(<Link href='/settings/roles'>Roles</Link>, 'set1.2', null),
+        getItem(<Link href='/settings/background-jobs'>Background Jobs</Link>, 'set1.3', null),
     ]),
 ];
 
---
+++ Moda.Web/src/moda.web.reactclient/app/settings/roles/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import PageTitle from "@/app/components/common/page-title";
+
+const Page = () => {
+  return (
+    <>
+      <PageTitle title="Roles"/>
+    </>
+  );
+}
+
+export default Page;
