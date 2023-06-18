1c8b274c398afeacc53958bc8279de64a0460646
Dannie Stacey <dstacey@outlook.com>
2023-06-18 14:13:15 -0500

    Added a grid control to allow users to include inactive employees in the dataset.

--- Moda.Web/src/moda.web.reactclient/src/app/organizations/employees/page.tsx
+++ Moda.Web/src/moda.web.reactclient/src/app/organizations/employees/page.tsx
@@ -5,9 +5,11 @@ import ModaGrid from "../../components/common/moda-grid";
 import { useEffect, useState } from "react";
 import { EmployeeListDto } from "@/src/services/moda-api";
 import { getEmployeesClient } from "@/src/services/clients";
+import { ItemType } from "antd/es/menu/hooks/useItems";
+import { Space, Switch } from "antd";
 
 const columnDefs = [
-  { field: 'localId', headerName: 'Id' },
+  { field: 'localId', headerName: '#' },
   { field: 'firstName' },
   { field: 'lastName' },
   { field: 'department' },
@@ -20,22 +22,35 @@ const columnDefs = [
 
 const Page = () => {
   const [employees, setEmployees] = useState<EmployeeListDto[]>([])
+  const [includeInactive, setIncludeInactive] = useState<boolean>(false)
+
+  const onIncludeInactiveChange = (checked: boolean) => {
+    setIncludeInactive(checked)
+  }
+
+  const controlItems: ItemType[] = [
+    {
+      label: <Space><Switch size="small" 
+                            checked={includeInactive} 
+                            onChange={onIncludeInactiveChange} />Include Inactive</Space>,
+      key: '0',
+    }
+  ];
 
   useEffect(() => {
     const getEmployees = async () => {
       const employeesClient = await getEmployeesClient()
-      const employeeDtos = await employeesClient.getList(false)
+      const employeeDtos = await employeesClient.getList(includeInactive)
       setEmployees(employeeDtos)
     }
 
     getEmployees()
-  }, [])
+  }, [includeInactive])
 
   return (
     <>
       <PageTitle title="Employees" />
-
-      <ModaGrid columnDefs={columnDefs}
+      <ModaGrid columnDefs={columnDefs} gridControlMenuItems={controlItems}
         rowData={employees} />
     </>
   );
