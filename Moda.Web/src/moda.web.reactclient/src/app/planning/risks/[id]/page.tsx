e638f9066c3b33e1cc48b76e4321fe6a416cb7a0
Dannie Stacey <dstacey@outlook.com>
2023-06-19 18:03:58 -0500

    Added the Risk details tab and layout.

--- Moda.Web/src/moda.web.reactclient/src/app/planning/risks/[id]/page.tsx
+++ Moda.Web/src/moda.web.reactclient/src/app/planning/risks/[id]/page.tsx
@@ -1,15 +1,41 @@
 'use client'
 
 import PageTitle from "@/src/app/components/common/page-title";
+import { getRisksClient } from "@/src/services/clients";
 import { RiskDetailsDto } from "@/src/services/moda-api";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import RiskDetails from "./risk-details";
+import React from "react";
+import { Card } from "antd";
 
 const Page = ({ params }) => {
+    const [activeTab, setActiveTab] = React.useState('details')
     const [risk, setRisk] = useState<RiskDetailsDto | null>(null)
+
+    const tabs = [
+        { key: 'details', tab: 'Details', content: React.createElement(RiskDetails, risk) },
+    ]
+
+    useEffect(() => {
+        const getRisk = async () => {
+            const risksClient = await getRisksClient()
+            const riskDto = await risksClient.getByLocalId(params.id)
+            setRisk(riskDto) // TODO: add sorting: by state: active, future, completed, then by start date
+        }
+
+        getRisk()
+    }, [])
 
     return (
         <>
-            <PageTitle title={risk?.summary ?? `Test ${params.id}` } subtitle="Risk Details" />
+            <PageTitle title={risk?.summary} subtitle="Risk Details" />
+            <Card style={{ width: '100%' }}
+                tabList={tabs}
+                activeTabKey={activeTab}
+                onTabChange={key => setActiveTab(key)}
+            >
+                {tabs.find(t => t.key === activeTab)?.content}
+            </Card>
         </>
     );
 }
---
+++ Moda.Web/src/moda.web.reactclient/src/app/planning/risks/[id]/risk-details.tsx
@@ -0,0 +1,40 @@
+import { RiskDetailsDto } from "@/src/services/moda-api"
+import { Col, Descriptions, Row } from "antd"
+import { ReactMarkdown } from "react-markdown/lib/react-markdown"
+
+const { Item } = Descriptions
+
+const RiskDetails = (risk: RiskDetailsDto) => {
+    return (
+        <>
+            <Row>
+                <Col xs={24} md={10}>
+                    <Descriptions layout="vertical">
+                        <Item label="Description"><ReactMarkdown>{risk.description}</ReactMarkdown></Item>
+                    </Descriptions>
+                    <Descriptions layout="vertical">
+                        <Item label="Response" ><ReactMarkdown>{risk.response}</ReactMarkdown></Item>
+                    </Descriptions>
+                </Col>
+                <Col xs={24} md={14}>
+                    <Descriptions>
+                        <Item label="Status">{risk.status?.name}</Item>
+                        <Item label="Team">{risk.team?.name}</Item>
+                        <Item label="Category">{risk.category?.name}</Item>
+                        <Item label="Follow-Up Date">{new Date(risk.followUpDate).toLocaleDateString()}</Item>
+                        <Item label="Assignee">{risk.assignee?.name}</Item>
+                        <Item label="exposure">{risk.exposure?.name}</Item>
+                        <Item label="Impact">{risk.impact?.name}</Item>
+                        <Item label="Likelihood">{risk.likelihood?.name}</Item>
+                    </Descriptions>
+                </Col>
+            </Row>
+            <Descriptions>
+                <Item label="Reported By">{risk.reportedBy?.name}</Item>
+                <Item label="Reported On">{new Date(risk.reportedOn).toLocaleDateString()}</Item>
+            </Descriptions>
+        </>
+    )
+}
+
+export default RiskDetails
