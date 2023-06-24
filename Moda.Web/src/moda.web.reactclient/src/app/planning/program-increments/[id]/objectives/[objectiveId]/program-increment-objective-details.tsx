cfb08d36150eccf8510a73a9766f04ddb5506e49
Dannie Stacey <dstacey@outlook.com>
2023-06-24 07:35:47 -0500

    Added the PI Objective Details page.

---
+++ Moda.Web/src/moda.web.reactclient/src/app/planning/program-increments/[id]/objectives/[objectiveId]/program-increment-objective-details.tsx
@@ -0,0 +1,64 @@
+import { ProgramIncrementObjectiveDetailsDto } from "@/src/services/moda-api";
+import { Col, Descriptions, Progress, Row, Tooltip } from "antd";
+import Link from "next/link";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+
+const { Item } = Descriptions;
+
+const ProgramIncrementObjectiveDetails = (
+    objective: ProgramIncrementObjectiveDetailsDto
+) => {
+    const startDate = objective.startDate
+        ? new Date(objective.startDate)?.toLocaleDateString()
+        : null;
+
+    const targetDate = objective.targetDate
+        ? new Date(objective.targetDate)?.toLocaleDateString()
+        : null;
+
+    const teamLink =
+        objective.team?.type === "Team"
+            ? `/organizations/teams/${objective.team?.localId}`
+            : `/organizations/team-of-teams/${objective.team?.localId}`;
+
+    return (
+        <>
+            <Row>
+                <Col span={12} offset={6}>
+                    <Tooltip title="Progress">
+                        <Progress percent={objective.progress} />
+                    </Tooltip>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={24} md={12}>
+                    <Descriptions>
+                        <Item label="Status">{objective.status?.name}</Item>
+                        <Item label="Is Stretch?">{objective.isStretch}</Item>
+                        <Item label="Team">
+                            <Link
+                                href={teamLink}
+                            >
+                                {objective.team?.name}
+                            </Link>
+                        </Item>
+                        <Item label="Type">{objective.type?.name}</Item>
+                        <Item label="Start">{startDate}</Item>
+                        <Item label="Target">{targetDate}</Item>
+                    </Descriptions>
+                </Col>
+                <Col xs={24} md={12}>
+                    <Descriptions layout="vertical">
+                        <Item label="Description">
+                            <ReactMarkdown>
+                                {objective.description}
+                            </ReactMarkdown>
+                        </Item>
+                    </Descriptions>
+                </Col>
+            </Row>
+        </>
+    );
+};
+
+export default ProgramIncrementObjectiveDetails;
