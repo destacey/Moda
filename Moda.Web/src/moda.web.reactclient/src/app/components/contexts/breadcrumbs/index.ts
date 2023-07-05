3053dad4c0e509eaffbde13202c0b630b32c3186
Dannie Stacey <dstacey@outlook.com>
2023-07-05 07:12:05 -0500

    Setup context and hook for managing breadcrumbs.

---
+++ Moda.Web/src/moda.web.reactclient/src/app/components/contexts/breadcrumbs/index.ts
@@ -0,0 +1,5 @@
+// import useBreadcrumbs from './use-breadcrumbs'
+
+// export { BreadcrumbsProvider, BreadcrumbsContext } from './breadcrumbs-context'
+// export type { BreadcrumbsContextType } from './types'
+// export default useBreadcrumbs
