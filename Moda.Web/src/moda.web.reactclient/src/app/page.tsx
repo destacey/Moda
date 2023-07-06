6143b8a325337cb982e49e23ad41f1c10273e219
Addison Waldow <addison.waldow@ahstech.com>
2023-07-06 15:30:01 -0700

    remove metadata export

--- Moda.Web/src/moda.web.reactclient/src/app/page.tsx
+++ Moda.Web/src/moda.web.reactclient/src/app/page.tsx
@@ -5,12 +5,6 @@ import ActiveProgramIncrements from './components/common/planning/active-program
 import MyAssignedRisks from './components/common/planning/my-assigned-risks'
 import { useDocumentTitle } from './hooks/use-document-title'
 
-export const metadata = {
-  title: {
-    absolute: 'Home',
-  },
-}
-
 const HomePage = () => {
   useDocumentTitle('Home')
 
