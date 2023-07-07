9bf19da385fef760711a27ed8fef50583ac22a03
Dannie Stacey <dstacey@outlook.com>
2023-07-06 23:48:57 -0500

    Created a team update form that can be used to update a Team or Team of Teams.

---
+++ Moda.Web/src/moda.web.reactclient/src/app/organizations/components/create-team/update-team-form.tsx
@@ -0,0 +1,249 @@
+'use client'
+
+import { Form, Input, Modal, message } from 'antd'
+import { useCallback, useEffect, useState } from 'react'
+import useAuth from '../../../components/contexts/auth'
+import { getTeamsClient, getTeamsOfTeamsClient } from '@/src/services/clients'
+import {
+  UpdateTeamOfTeamsRequest,
+  UpdateTeamRequest,
+} from '@/src/services/moda-api'
+import { toFormErrors } from '@/src/utils'
+
+export interface UpdateTeamFormProps {
+  showForm: boolean
+  localId: number
+  type: string
+  onFormUpdate: () => void
+  onFormCancel: () => void
+}
+
+interface UpdateTeamFormValues {
+  id: string
+  name: string
+  code: string
+  description: string
+}
+
+const UpdateTeamForm = ({
+  showForm,
+  localId,
+  type,
+  onFormUpdate,
+  onFormCancel,
+}: UpdateTeamFormProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [isSaving, setIsSaving] = useState(false)
+  const [isValid, setIsValid] = useState(false)
+  const [form] = Form.useForm<UpdateTeamFormValues>()
+  const formValues = Form.useWatch([], form)
+  const [messageApi, contextHolder] = message.useMessage()
+
+  const { hasClaim } = useAuth()
+  const canUpdateTeam = hasClaim('Permission', 'Permissions.Teams.Update')
+
+  const mapTeamToFormValues = useCallback(
+    (team: UpdateTeamFormValues) => {
+      form.setFieldsValue({
+        id: team.id,
+        name: team.name,
+        code: team.code,
+        description: team.description,
+      })
+    },
+    [form]
+  )
+
+  const getTeamData = useCallback(async (teamLocalId: number) => {
+    const teamsClient = await getTeamsClient()
+    return (await teamsClient.getById(teamLocalId)) as UpdateTeamFormValues
+  }, [])
+
+  const getTeamOfTeamsData = useCallback(async (teamLocalId: number) => {
+    const teamsOfTeamsClient = await getTeamsOfTeamsClient()
+    return (await teamsOfTeamsClient.getById(
+      teamLocalId
+    )) as UpdateTeamFormValues
+  }, [])
+
+  const loadData = useCallback(async () => {
+    try {
+      let teamData: UpdateTeamFormValues = null
+      if (type === 'Team') {
+        teamData = await getTeamData(localId)
+      } else if (type === 'Team of Teams') {
+        teamData = await getTeamOfTeamsData(localId)
+      } else {
+        throw new Error(`Invalid team type: ${type}`)
+      }
+      mapTeamToFormValues(teamData)
+    } catch (error) {
+      messageApi.error(
+        `An unexpected error occurred while retrieving the ${type}.`
+      )
+      console.error(error)
+    }
+  }, [
+    localId,
+    mapTeamToFormValues,
+    messageApi,
+    getTeamData,
+    getTeamOfTeamsData,
+    type,
+  ])
+
+  useEffect(() => {
+    if (canUpdateTeam) {
+      setIsOpen(showForm)
+    } else {
+      onFormCancel()
+      messageApi.error('You do not have permission to update teams.')
+    }
+
+    if (showForm) {
+      loadData()
+    }
+  }, [canUpdateTeam, loadData, messageApi, onFormCancel, showForm])
+
+  useEffect(() => {
+    form.validateFields({ validateOnly: true }).then(
+      () => setIsValid(true && form.isFieldsTouched()),
+      () => setIsValid(false)
+    )
+  }, [form, formValues])
+
+  const update = async (values: UpdateTeamFormValues): Promise<boolean> => {
+    try {
+      if (type === 'Team') {
+        await updateTeam(values)
+      } else if (type === 'Team of Teams') {
+        await updateTeamOfTeams(values)
+      } else {
+        throw new Error(`Invalid team type: ${type}`)
+      }
+      return true
+    } catch (error) {
+      if (error.status === 422 && error.errors) {
+        const formErrors = toFormErrors(error.errors)
+        form.setFields(formErrors)
+        messageApi.error('Correct the validation error(s) to continue.')
+      } else {
+        messageApi.error(
+          `An unexpected error occurred while creating the ${type}.`
+        )
+        console.error(error)
+      }
+
+      return false
+    }
+  }
+
+  const updateTeam = async (values: UpdateTeamFormValues) => {
+    const teamsClient = await getTeamsClient()
+    await teamsClient.update(values.id, values as UpdateTeamRequest)
+  }
+
+  const updateTeamOfTeams = async (values: UpdateTeamFormValues) => {
+    const teamsOfTeamsClient = await getTeamsOfTeamsClient()
+    await teamsOfTeamsClient.update(
+      values.id,
+      values as UpdateTeamOfTeamsRequest
+    )
+  }
+
+  const handleOk = async () => {
+    setIsSaving(true)
+    try {
+      const values = await form.validateFields()
+      if (await update(values)) {
+        setIsOpen(false)
+        setIsSaving(false)
+        form.resetFields()
+        onFormUpdate()
+        messageApi.success(`Successfully updated ${type}.`)
+      } else {
+        setIsSaving(false)
+      }
+    } catch (errorInfo) {
+      setIsSaving(false)
+    }
+  }
+
+  const handleCancel = () => {
+    setIsOpen(false)
+    onFormCancel()
+    form.resetFields()
+  }
+
+  return (
+    <>
+      {contextHolder}
+      <Modal
+        title="Edit Team"
+        open={isOpen}
+        onOk={handleOk}
+        okButtonProps={{ disabled: !isValid }}
+        okText="Save"
+        confirmLoading={isSaving}
+        onCancel={handleCancel}
+        maskClosable={false}
+        closable={false}
+        destroyOnClose={true}
+      >
+        <Form
+          form={form}
+          size="small"
+          layout="vertical"
+          name="update-team-form"
+        >
+          <Form.Item name="id" hidden={true}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="Name" name="name" rules={[{ required: true }]}>
+            <Input showCount maxLength={128} />
+          </Form.Item>
+          <Form.Item
+            label="Code"
+            name="code"
+            rules={[
+              { required: true, message: 'The Code field is required.' },
+              {
+                min: 2,
+                max: 10,
+                message: 'The Code field must be between 2-10 characters.',
+              },
+              {
+                pattern: /^[A-Z0-9]+$/,
+                message:
+                  'The Code field is invalid. Uppercase letters and numbers only.',
+              },
+            ]}
+          >
+            <Input
+              showCount
+              maxLength={10}
+              onInput={(e) =>
+                ((e.target as HTMLInputElement).value = (
+                  e.target as HTMLInputElement
+                ).value.toUpperCase())
+              }
+            />
+          </Form.Item>
+          <Form.Item
+            name="description"
+            label="Description"
+            help="Markdown enabled"
+          >
+            <Input.TextArea
+              autoSize={{ minRows: 6, maxRows: 10 }}
+              showCount
+              maxLength={1024}
+            />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  )
+}
+
+export default UpdateTeamForm
