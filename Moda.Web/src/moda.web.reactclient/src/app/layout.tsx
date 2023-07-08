27d137eacba5471737c07c9df8b87a56e38dc7fe
Dannie Stacey <dstacey@outlook.com>
2023-07-08 07:17:18 -0500

    Implemented the new useMenuToggle hook and context.

--- Moda.Web/src/moda.web.reactclient/src/app/layout.tsx
+++ Moda.Web/src/moda.web.reactclient/src/app/layout.tsx
@@ -2,7 +2,7 @@
 
 import '../../styles/globals.css'
 import React from 'react'
-import { Layout } from 'antd'
+import { Layout, Menu } from 'antd'
 import { AuthenticatedTemplate } from '@azure/msal-react'
 import AppHeader from './components/common/app-header'
 import AppMenu from './components/common/menu'
@@ -10,6 +10,7 @@ import AppBreadcrumb from './components/common/app-breadcrumb'
 import { ThemeProvider } from './components/contexts/theme'
 import { AuthProvider } from './components/contexts/auth'
 import { BreadcrumbsProvider } from './components/contexts/breadcrumbs'
+import { MenuToggleProvider } from './components/contexts/menu-toggle'
 
 const { Content } = Layout
 
@@ -18,32 +19,33 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-
   return (
     <html lang="en">
       <body>
         <AuthProvider>
           <ThemeProvider>
             <AuthenticatedTemplate>
-              <Layout className="layout" style={{ minHeight: '100vh' }}>
-                <AppHeader />
-                <Layout>
-                  <AppMenu />
-                  <Layout style={{ padding: '0 24px 24px' }}>
-                  <BreadcrumbsProvider>
-                      <AppBreadcrumb />
-                      <Content
-                        style={{
-                          margin: 0,
-                          minHeight: 280,
-                        }}
-                      >
-                        {children}
-                      </Content>
-                    </BreadcrumbsProvider>
+              <MenuToggleProvider>
+                <Layout className="layout" style={{ minHeight: '100vh' }}>
+                  <AppHeader />
+                  <Layout>
+                    <AppMenu />
+                    <Layout style={{ padding: '0 24px 24px' }}>
+                      <BreadcrumbsProvider>
+                        <AppBreadcrumb />
+                        <Content
+                          style={{
+                            margin: 0,
+                            minHeight: 280,
+                          }}
+                        >
+                          {children}
+                        </Content>
+                      </BreadcrumbsProvider>
+                    </Layout>
                   </Layout>
                 </Layout>
-              </Layout>
+              </MenuToggleProvider>
             </AuthenticatedTemplate>
           </ThemeProvider>
         </AuthProvider>
