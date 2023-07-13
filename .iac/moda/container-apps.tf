e92962ca27b98c6820537b503b1087e27811b76e
Addison Waldow <addison.waldow@ahstech.com>
2023-07-12 17:51:22 -0700

    fix cors settings

--- .iac/moda/container-apps.tf
+++ .iac/moda/container-apps.tf
@@ -210,7 +210,7 @@ resource "azurerm_container_app" "moda_backend" {
 
       env {
         name  = "CorsSettings__WebClient"
-        value = "https://${azurerm_static_site.moda_swa.default_host_name},https://${azurerm_container_app.moda_frontend.ingress.0.fqdn}"
+        value = "https://${azurerm_static_site.moda_swa.default_host_name};https://${azurerm_container_app.moda_frontend.ingress.0.fqdn}"
       }
     }
   }
