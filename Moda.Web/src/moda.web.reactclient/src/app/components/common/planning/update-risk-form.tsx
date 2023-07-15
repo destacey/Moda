92e7031d92096dc5afdd72fc88e6d366a48c2599
Dannie Stacey <dstacey@outlook.com>
2023-07-15 09:33:24 -0500

    Finished the update risk form.

--- Moda.Web/src/moda.web.reactclient/src/app/components/common/planning/update-risk-form.tsx
+++ Moda.Web/src/moda.web.reactclient/src/app/components/common/planning/update-risk-form.tsx
@@ -1,22 +1,27 @@
 'use client'
 
-import { DatePicker, Form, Input, Modal, Radio, Select, message } from 'antd'
+import {
+  DatePicker,
+  Descriptions,
+  Form,
+  Input,
+  Modal,
+  Radio,
+  Select,
+  message,
+} from 'antd'
 import { useCallback, useEffect, useState } from 'react'
 import useAuth from '../../contexts/auth'
-import {
-  getEmployeesClient,
-  getRisksClient,
-  getTeamsClient,
-  getTeamsOfTeamsClient,
-} from '@/src/services/clients'
-import { UpdateRiskRequest } from '@/src/services/moda-api'
+import { getEmployeesClient, getRisksClient } from '@/src/services/clients'
+import { RiskDetailsDto, UpdateRiskRequest } from '@/src/services/moda-api'
 import { toFormErrors } from '@/src/utils'
-import { TeamListItem } from '@/src/app/organizations/types'
 import _ from 'lodash'
+import dayjs from 'dayjs'
 
 export interface UpdateRiskFormProps {
   showForm: boolean
-  onFormUpdate: () => void
+  riskId: string
+  onFormSave: () => void
   onFormCancel: () => void
 }
 
@@ -25,23 +30,24 @@ interface UpdateRiskFormValues {
   teamId: string
   summary: string
   description?: string | undefined
-  categoryId?: number
-  impactId?: number
-  likelihoodId?: number
+  statusId: number
+  categoryId: number
+  impactId: number
+  likelihoodId: number
   assigneeId?: string | undefined
   followUpDate?: Date | undefined
   response?: string | undefined
 }
 
-interface OptionModel {
-  value: string
+interface OptionModel<T = string> {
+  value: T
   label: string
 }
 
 const UpdateRiskForm = ({
   showForm,
-  UpdateForTeamId,
-  onFormUpdate,
+  riskId,
+  onFormSave,
   onFormCancel,
 }: UpdateRiskFormProps) => {
   const [isOpen, setIsOpen] = useState(false)
@@ -51,20 +57,31 @@ const UpdateRiskForm = ({
   const formValues = Form.useWatch([], form)
   const [messageApi, contextHolder] = message.useMessage()
 
-  const [teamOptions, setTeamOptions] = useState<OptionModel[]>()
+  const [riskNumber, setRiskNumber] = useState<number>(undefined)
+  const [teamName, setTeamName] = useState<string>('')
   const [employeeOptions, setEmployeeOptions] = useState<OptionModel[]>()
-  const [categoryOptions, setCategoryOptions] = useState<OptionModel[]>()
-  const [gradeOptions, setGradeOptions] = useState<OptionModel[]>()
-
-  const [newRiskLocalId, setNewRiskLocalId] = useState<number>(null)
+  const [statusOptions, setStatusOptions] = useState<OptionModel<number>[]>()
+  const [categoryOptions, setCategoryOptions] =
+    useState<OptionModel<number>[]>()
+  const [gradeOptions, setGradeOptions] = useState<OptionModel<number>[]>()
 
   const { hasClaim } = useAuth()
   const canUpdateRisks = hasClaim('Permission', 'Permissions.Risks.Update')
 
   const mapToFormValues = useCallback(
-    (teamId: string | undefined) => {
+    (risk: RiskDetailsDto) => {
       form.setFieldsValue({
-        teamId: teamId,
+        riskId: risk.id,
+        teamId: risk.team.id,
+        summary: risk.summary,
+        description: risk.description,
+        statusId: risk.status.id,
+        categoryId: risk.category.id,
+        impactId: risk.impact.id,
+        likelihoodId: risk.likelihood.id,
+        assigneeId: risk.assignee.id,
+        followUpDate: risk.followUpDate ? dayjs(risk.followUpDate) : undefined,
+        response: risk.response,
       })
     },
     [form]
@@ -72,9 +89,11 @@ const UpdateRiskForm = ({
 
   const mapToRequestValues = useCallback((values: UpdateRiskFormValues) => {
     return {
+      riskId: values.riskId,
       teamId: values.teamId,
       summary: values.summary,
       description: values.description,
+      statusId: values.statusId,
       categoryId: values.categoryId,
       impactId: values.impactId,
       likelihoodId: values.likelihoodId,
@@ -84,17 +103,9 @@ const UpdateRiskForm = ({
     } as UpdateRiskRequest
   }, [])
 
-  const getTeamOptions = useCallback(async () => {
-    const teamsClient = await getTeamsClient()
-    const teamsDtos = await teamsClient.getList(false)
-    const teamOfTeamsClient = await getTeamsOfTeamsClient()
-    const teamOfTeamsDtos = await teamOfTeamsClient.getList(false)
-    const teams: OptionModel[] = [
-      ...(teamsDtos as TeamListItem[]),
-      ...(teamOfTeamsDtos as TeamListItem[]),
-    ].map((t) => ({ value: t.id, label: t.name }))
-
-    return _.sortBy(teams, ['label'])
+  const getRisk = useCallback(async (riskId: string) => {
+    const risksClient = await getRisksClient()
+    return await risksClient.getById(riskId)
   }, [])
 
   const getEmployeeOptions = useCallback(async () => {
@@ -107,11 +118,22 @@ const UpdateRiskForm = ({
     return _.sortBy(employees, ['label'])
   }, [])
 
-  const getRiskCategorOptions = useCallback(async () => {
+  const getRiskStatusOptions = useCallback(async () => {
+    const risksClient = await getRisksClient()
+    const statusDtos = await risksClient.getStatuses()
+    const statuses: OptionModel<number>[] = statusDtos.map((r) => ({
+      value: r.id,
+      label: r.name,
+    }))
+
+    return _.sortBy(statuses, ['order'])
+  }, [])
+
+  const getRiskCategoryOptions = useCallback(async () => {
     const risksClient = await getRisksClient()
     const categoryDtos = await risksClient.getCategories()
-    const categories: OptionModel[] = categoryDtos.map((r) => ({
-      value: r.id.toString(),
+    const categories: OptionModel<number>[] = categoryDtos.map((r) => ({
+      value: r.id,
       label: r.name,
     }))
 
@@ -121,21 +143,20 @@ const UpdateRiskForm = ({
   const getRiskGradeOptions = useCallback(async () => {
     const risksClient = await getRisksClient()
     const gradeDtos = await risksClient.getGrades()
-    const grades: OptionModel[] = gradeDtos.map((r) => ({
-      value: r.id.toString(),
+    const grades: OptionModel<number>[] = gradeDtos.map((r) => ({
+      value: r.id,
       label: r.name,
     }))
 
     return _.sortBy(grades, ['order'])
   }, [])
 
-  const Update = useCallback(
+  const update = useCallback(
     async (values: UpdateRiskFormValues) => {
       try {
         const request = mapToRequestValues(values)
         const risksClient = await getRisksClient()
-        const localId = await risksClient.UpdateRisk(request)
-        setNewRiskLocalId(localId)
+        await risksClient.update(request.riskId, request)
 
         return true
       } catch (error) {
@@ -145,7 +166,7 @@ const UpdateRiskForm = ({
           messageApi.error('Correct the validation error(s) to continue.')
         } else {
           messageApi.error(
-            'An unexpected error occurred while creating the Risk.'
+            'An unexpected error occurred while updating the Risk.'
           )
           console.error(error)
         }
@@ -159,13 +180,18 @@ const UpdateRiskForm = ({
     setIsSaving(true)
     try {
       const values = await form.validateFields()
-      await Update(values)
+      if (await update(values)) {
+        setIsOpen(false)
+        onFormSave()
+        form.resetFields()
+        messageApi.success('Successfully updated risk.')
+      }
     } catch (errorInfo) {
       console.log('handleOk error', errorInfo)
     } finally {
       setIsSaving(false)
     }
-  }, [form, Update])
+  }, [form, messageApi, onFormSave, update])
 
   const handleCancel = useCallback(() => {
     setIsOpen(false)
@@ -179,13 +205,16 @@ const UpdateRiskForm = ({
       if (showForm === true) {
         try {
           const loadData = async () => {
-            setTeamOptions(await getTeamOptions())
+            const riskData = await getRisk(riskId)
+            setRiskNumber(riskData.localId)
+            setTeamName(riskData.team.name)
+            mapToFormValues(riskData)
             setEmployeeOptions(await getEmployeeOptions())
-            setCategoryOptions(await getRiskCategorOptions())
+            setStatusOptions(await getRiskStatusOptions())
+            setCategoryOptions(await getRiskCategoryOptions())
             setGradeOptions(await getRiskGradeOptions())
           }
           loadData()
-          mapToFormValues(UpdateForTeamId)
         } catch (error) {
           handleCancel()
           messageApi.error(
@@ -196,18 +225,18 @@ const UpdateRiskForm = ({
       }
     } else {
       handleCancel()
-      messageApi.error('You do not have permission to Update Risks.')
+      messageApi.error('You do not have permission to update Risks.')
     }
   }, [
     canUpdateRisks,
-    UpdateForTeamId,
     getEmployeeOptions,
-    getRiskCategorOptions,
+    getRisk,
+    getRiskCategoryOptions,
     getRiskGradeOptions,
-    getTeamOptions,
     handleCancel,
     mapToFormValues,
     messageApi,
+    riskId,
     showForm,
   ])
 
@@ -218,26 +247,15 @@ const UpdateRiskForm = ({
     )
   }, [form, formValues])
 
-  useEffect(() => {
-    if (newRiskLocalId) {
-      setIsOpen(false)
-      form.resetFields()
-      onFormUpdate()
-      messageApi.success('Successfully Updated Risk.')
-    }
-    // we don't want a trigger on the other dependencies
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newRiskLocalId])
-
   return (
     <>
       {contextHolder}
       <Modal
-        title="Update Team Risk"
+        title="Edit Team Risk"
         open={isOpen}
         onOk={handleOk}
         okButtonProps={{ disabled: !isValid }}
-        okText="Update"
+        okText="Save"
         confirmLoading={isSaving}
         onCancel={handleCancel}
         maskClosable={false}
@@ -249,26 +267,17 @@ const UpdateRiskForm = ({
           form={form}
           size="small"
           layout="vertical"
-          name="Update-risk-form"
+          name="update-risk-form"
         >
-          <Form.Item name="teamId" label="Team" rules={[{ required: true }]}>
-            <Select
-              showSearch
-              disabled={UpdateForTeamId !== undefined}
-              placeholder="Select a team"
-              optionFilterProp="children"
-              filterOption={(input, option) =>
-                (option?.label.toLowerCase() ?? '').includes(
-                  input.toLowerCase()
-                )
-              }
-              filterSort={(optionA, optionB) =>
-                (optionA?.label ?? '')
-                  .toLowerCase()
-                  .localeCompare((optionB?.label ?? '').toLowerCase())
-              }
-              options={teamOptions}
-            />
+          <Descriptions size="small" column={1}>
+            <Descriptions.Item label="Number">{riskNumber}</Descriptions.Item>
+            <Descriptions.Item label="Team">{teamName}</Descriptions.Item>
+          </Descriptions>
+          <Form.Item name="riskId" hidden={true}>
+            <Input />
+          </Form.Item>
+          <Form.Item name="teamId" hidden={true}>
+            <Input />
           </Form.Item>
           <Form.Item
             label="Summary"
@@ -286,6 +295,17 @@ const UpdateRiskForm = ({
               autoSize={{ minRows: 6, maxRows: 10 }}
               showCount
               maxLength={1024}
+            />
+          </Form.Item>
+          <Form.Item
+            name="statusId"
+            label="Status"
+            rules={[{ required: true }]}
+          >
+            <Radio.Group
+              options={statusOptions}
+              optionType="button"
+              buttonStyle="solid"
             />
           </Form.Item>
           <Form.Item
