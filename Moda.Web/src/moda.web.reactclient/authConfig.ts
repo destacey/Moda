93fd3a00fde2491e54b1b2182a7c900d5458c138
Dannie Stacey <dstacey@outlook.com>
2023-07-26 19:57:23 -0500

    Commenting out some of the auth logs.

--- Moda.Web/src/moda.web.reactclient/authConfig.ts
+++ Moda.Web/src/moda.web.reactclient/authConfig.ts
@@ -23,12 +23,12 @@ export const msalConfig: Configuration = {
           case LogLevel.Error:
             console.error(message)
             return
-          case LogLevel.Info:
-            console.info(message)
-            return
-          case LogLevel.Verbose:
-            console.debug(message)
-            return
+          // case LogLevel.Info:
+          //   console.info(message)
+          //   return
+          // case LogLevel.Verbose:
+          //   console.debug(message)
+          //   return
           case LogLevel.Warning:
             console.warn(message)
             return
