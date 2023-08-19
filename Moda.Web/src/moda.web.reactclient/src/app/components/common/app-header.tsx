521ee728d790a05cd73b93d60e32e784625d59a7
Dannie Stacey <dstacey@outlook.com>
2023-08-19 09:44:23 -0500

    Updated the app-header component to use antd components so the theme is applied correctly.

--- Moda.Web/src/moda.web.reactclient/src/app/components/common/app-header.tsx
+++ Moda.Web/src/moda.web.reactclient/src/app/components/common/app-header.tsx
@@ -11,28 +11,32 @@ export default function AppHeader() {
   const { appBarColor } = useTheme()
 
   return (
-    <Header
-      style={{
-        height: 50,
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        backgroundColor: appBarColor,
-      }}
-    >
-      <Space>
-        {React.createElement(
-          menuCollapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-          {
-            style: { fontSize: '20px', padding: '27px 0px 0px 0px' },
-            onClick: () => setMenuCollapsed(!menuCollapsed),
-          }
-        )}
-        <Typography.Title style={{ margin: 0, fontSize: 24, fontWeight: 400 }}>
-          Moda
-        </Typography.Title>
-      </Space>
-      <Profile />
-    </Header>
+    <>
+      <Header
+        style={{
+          height: 50,
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          backgroundColor: appBarColor,
+        }}
+      >
+        <Space>
+          <Button
+            type="text"
+            shape="default"
+            size="middle"
+            icon={menuCollapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setMenuCollapsed(!menuCollapsed)}
+          />
+          <Typography.Title
+            style={{ margin: 0, fontSize: 24, fontWeight: 400 }}
+          >
+            Moda
+          </Typography.Title>
+        </Space>
+        <Profile />
+      </Header>
+    </>
   )
 }
