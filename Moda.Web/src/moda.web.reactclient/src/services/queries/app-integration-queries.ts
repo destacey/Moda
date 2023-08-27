92975e003ae2dc81cdfd5402fae387a17c34069f
Dannie Stacey <dstacey@outlook.com>
2023-08-27 16:52:56 -0500

    Added the ability to edit an existing connection.

--- Moda.Web/src/moda.web.reactclient/src/services/queries/app-integration-queries.ts
+++ Moda.Web/src/moda.web.reactclient/src/services/queries/app-integration-queries.ts
@@ -1,7 +1,10 @@
 import { useMutation, useQuery, useQueryClient } from 'react-query'
 import { QK } from './query-keys'
 import { getAzureDevOpsBoardsConnectionsClient } from '../clients'
-import { CreateAzureDevOpsBoardConnectionRequest } from '../moda-api'
+import {
+  CreateAzureDevOpsBoardConnectionRequest,
+  UpdateAzureDevOpsBoardConnectionRequest,
+} from '../moda-api'
 
 // CONNECTIONS
 export const useGetConnections = (includeDisabled: boolean = false) => {
@@ -29,6 +32,22 @@ export const useCreateConnectionMutation = () => {
       (await getAzureDevOpsBoardsConnectionsClient()).create(connection),
     onSuccess: () => {
       queryClient.invalidateQueries([QK.CONNECTIONS, false])
+    },
+  })
+}
+
+export const useUpdateConnectionMutation = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: async (connection: UpdateAzureDevOpsBoardConnectionRequest) =>
+      (await getAzureDevOpsBoardsConnectionsClient()).update(
+        connection.id,
+        connection,
+      ),
+    onSuccess: (data, context) => {
+      queryClient.invalidateQueries([QK.CONNECTIONS, false])
+      queryClient.invalidateQueries([QK.CONNECTIONS, true])
+      queryClient.invalidateQueries([QK.CONNECTIONS, context.id])
     },
   })
 }
