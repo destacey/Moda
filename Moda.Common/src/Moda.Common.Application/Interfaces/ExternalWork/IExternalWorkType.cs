efcb0ea6356e22a7a5361fcd85e131c69d1380ea
Dannie Stacey <dstacey@outlook.com>
2023-09-04 09:25:19 -0500

    Added the ability to test an Azure DevOps Connection.

---
+++ Moda.Common/src/Moda.Common.Application/Interfaces/ExternalWork/IExternalWorkType.cs
@@ -0,0 +1,7 @@
+﻿namespace Moda.Common.Application.Interfaces.ExternalWork;
+public interface IExternalWorkType
+{
+    string Name { get; }
+    string ReferenceName { get; }
+    string Description { get; }
+}
---
+++ Moda.Integrations/src/Moda.Integrations.AzureDevOps/Models/AzdoWorkspace.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.TeamFoundation.Core.WebApi;
+using Moda.Common.Application.Interfaces.ExternalWork;
+
+namespace Moda.Integrations.AzureDevOps.Models;
+public class AzdoWorkspace : IExternalWorkspace
+{
+    public AzdoWorkspace(TeamProject teamProject)
+    {
+        Id = teamProject.Id;
+        Name = teamProject.Name;
+        Abbreviation = teamProject.Name;
+        Description = teamProject.Description;
+    }
+
+    public AzdoWorkspace(TeamProjectReference teamProject)
+    {
+        Id = teamProject.Id;
+        Name = teamProject.Name;
+        Abbreviation = teamProject.Name;
+        Description = teamProject.Description;
+    }
+
+    public Guid Id { get; private set; }
+
+    public string Name { get; private set; }
+
+    public string Abbreviation { get; private set; }
+
+    public string? Description { get; private set; }
+
+}
