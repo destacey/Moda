707b663222bae7422042797df31ed97d619162cf
Dannie Stacey <dstacey@outlook.com>
2023-09-10 13:44:56 -0500

    Added an alert message when no workspaces exist for a connection.

--- Moda.Web/src/moda.web.reactclient/src/app/settings/connections/[id]/azdo-boards-workspaces.tsx
+++ Moda.Web/src/moda.web.reactclient/src/app/settings/connections/[id]/azdo-boards-workspaces.tsx
@@ -1,6 +1,6 @@
 import { AzureDevOpsBoardsWorkspaceDto } from '@/src/services/moda-api'
 import { ExportOutlined } from '@ant-design/icons'
-import { Card, List, Space, Typography } from 'antd'
+import { Alert, Card, List, Space, Typography } from 'antd'
 import Link from 'next/link'
 
 interface AzdoBoardsWorkspacesProps {
@@ -13,14 +13,6 @@ const AzdoBoardsWorkspaces = ({
   organizationUrl,
 }: AzdoBoardsWorkspacesProps) => {
   if (!workspaces) return null
-  if (workspaces.length === 0)
-    return (
-      <>
-        <Typography.Text>
-          The workspaces below represent projects within Azure DevOps.
-        </Typography.Text>
-      </>
-    )
 
   return (
     <>
@@ -28,39 +20,46 @@ const AzdoBoardsWorkspaces = ({
         <Typography.Text>
           The workspaces below represent projects within Azure DevOps.
         </Typography.Text>
-        <List
-          grid={{
-            gutter: 16,
-            xs: 1,
-            sm: 1,
-            md: 2,
-            lg: 3,
-            xl: 4,
-            xxl: 6,
-          }}
-          dataSource={workspaces}
-          renderItem={(item) => (
-            <List.Item>
-              <Card
-                title={
-                  <>
-                    {item.name}{' '}
-                    <Link
-                      href={`${organizationUrl}/${item.name}`}
-                      target="_blank"
-                      title="Open in Azure DevOps"
-                    >
-                      <ExportOutlined style={{ width: '12px' }} />
-                    </Link>
-                  </>
-                }
-              >
-                {/* adds a non-breaking space if the description is null */}
-                {item.description ?? '\u00A0'}
-              </Card>
-            </List.Item>
-          )}
-        />
+        {workspaces.length === 0 ? (
+          <Alert
+            message="No workspaces were found for this connection."
+            type="error"
+          />
+        ) : (
+          <List
+            grid={{
+              gutter: 16,
+              xs: 1,
+              sm: 1,
+              md: 2,
+              lg: 3,
+              xl: 4,
+              xxl: 6,
+            }}
+            dataSource={workspaces}
+            renderItem={(item) => (
+              <List.Item>
+                <Card
+                  title={
+                    <>
+                      {item.name}{' '}
+                      <Link
+                        href={`${organizationUrl}/${item.name}`}
+                        target="_blank"
+                        title="Open in Azure DevOps"
+                      >
+                        <ExportOutlined style={{ width: '12px' }} />
+                      </Link>
+                    </>
+                  }
+                >
+                  {/* adds a non-breaking space if the description is null */}
+                  {item.description ?? '\u00A0'}
+                </Card>
+              </List.Item>
+            )}
+          />
+        )}
       </Space>
     </>
   )
