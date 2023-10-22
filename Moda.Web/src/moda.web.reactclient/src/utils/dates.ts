e1f3d2906b09303226dea7d614580fa36dfad69c
Dannie Stacey <dstacey@outlook.com>
2023-10-22 07:43:34 -0500

    Updated the countdown logic and what's displayed for the PI.

--- Moda.Web/src/moda.web.reactclient/src/utils/dates.ts
+++ Moda.Web/src/moda.web.reactclient/src/utils/dates.ts
@@ -1,4 +1,4 @@
-export function programIncrementDaysRemaining(endDate: Date): number {
+export function daysRemaining(endDate: Date): number {
   const end = new Date(endDate)
   const now = new Date()
   return Math.ceil((end.getTime() - now.getTime()) / (1000 * 3600 * 24))
