f5662bb038e8e9f5d4873915ee4074f11f47f0c6
Dannie Stacey <dstacey@outlook.com>
2023-12-20 09:57:50 -0600

    Added a missing migration from the Program Increment to Planning Interval rename.

---
+++ Moda.Infrastructure/src/Moda.Infrastructure.Migrators.MSSQL/Migrations/20231220155401_Rename-HealthCheck-Context-Values.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Moda.Infrastructure.Migrators.MSSQL.Migrations;
+
+/// <inheritdoc />
+public partial class RenameHealthCheckContextValues : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+            UPDATE [Health].[HealthChecks]
+            SET [Context] = 'PlanningPlanningIntervalObjective'
+            WHERE [Context] = 'PlanningProgramIncrementObjective'");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+            UPDATE [Health].[HealthChecks]
+            SET [Context] = 'PlanningProgramIncrementObjective'
+            WHERE [Context] = 'PlanningPlanningIntervalObjective'");
+    }
+}
