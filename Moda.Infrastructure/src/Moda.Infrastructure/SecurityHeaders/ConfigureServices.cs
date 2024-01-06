9d764431183c52d00d6d869932f8b85e4b7eaece
Dannie Stacey <dstacey@outlook.com>
2024-01-06 09:20:03 -0600

    refactor to a better method

--- Moda.Infrastructure/src/Moda.Infrastructure/SecurityHeaders/ConfigureServices.cs
+++ Moda.Infrastructure/src/Moda.Infrastructure/SecurityHeaders/ConfigureServices.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 
 namespace Moda.Infrastructure.SecurityHeaders;
@@ -15,32 +16,32 @@ internal static class ConfigureServices
             {
                 if (!string.IsNullOrWhiteSpace(settings.XFrameOptions))
                 {
-                    context.Response.Headers.Add(HeaderNames.XFRAMEOPTIONS, settings.XFrameOptions);
+                    context.Response.Headers.Append(HeaderNames.XFRAMEOPTIONS, settings.XFrameOptions);
                 }
 
                 if (!string.IsNullOrWhiteSpace(settings.XContentTypeOptions))
                 {
-                    context.Response.Headers.Add(HeaderNames.XCONTENTTYPEOPTIONS, settings.XContentTypeOptions);
+                    context.Response.Headers.Append(HeaderNames.XCONTENTTYPEOPTIONS, settings.XContentTypeOptions);
                 }
 
                 if (!string.IsNullOrWhiteSpace(settings.ReferrerPolicy))
                 {
-                    context.Response.Headers.Add(HeaderNames.REFERRERPOLICY, settings.ReferrerPolicy);
+                    context.Response.Headers.Append(HeaderNames.REFERRERPOLICY, settings.ReferrerPolicy);
                 }
 
                 if (!string.IsNullOrWhiteSpace(settings.PermissionsPolicy))
                 {
-                    context.Response.Headers.Add(HeaderNames.PERMISSIONSPOLICY, settings.PermissionsPolicy);
+                    context.Response.Headers.Append(HeaderNames.PERMISSIONSPOLICY, settings.PermissionsPolicy);
                 }
 
                 if (!string.IsNullOrWhiteSpace(settings.SameSite))
                 {
-                    context.Response.Headers.Add(HeaderNames.SAMESITE, settings.SameSite);
+                    context.Response.Headers.Append(HeaderNames.SAMESITE, settings.SameSite);
                 }
 
                 if (!string.IsNullOrWhiteSpace(settings.XXSSProtection))
                 {
-                    context.Response.Headers.Add(HeaderNames.XXSSPROTECTION, settings.XXSSProtection);
+                    context.Response.Headers.Append(HeaderNames.XXSSPROTECTION, settings.XXSSProtection);
                 }
 
                 await next();
