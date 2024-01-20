f2976c0341df63b813a6a34fdb8d61fda5dea1a4
Dannie Stacey <dstacey@outlook.com>
2024-01-20 09:12:45 -0600

    Replaced custom request and response logging with Serilog's request logging.

--- Moda.Infrastructure/src/Moda.Infrastructure/Middleware/MiddlewareSettings.cs
+++ Moda.Infrastructure/src/Moda.Infrastructure/Middleware/MiddlewareSettings.cs
@@ -2,5 +2,5 @@ namespace Moda.Infrastructure.Middleware;
 
 public class MiddlewareSettings
 {
-    public bool EnableHttpsLogging { get; set; } = false;
+    public bool EnableHttpsLogging { get; set; } = true;
 }
--- Moda.Infrastructure/src/Moda.Infrastructure/Middleware/RequestLoggingMiddleware.cs
+++ Moda.Infrastructure/src/Moda.Infrastructure/Middleware/RequestLoggingMiddleware.cs
@@ -5,53 +5,19 @@ using Serilog.Context;
 
 namespace Moda.Infrastructure.Middleware;
 
-public class RequestLoggingMiddleware : IMiddleware
+public class RequestLoggingMiddleware(ICurrentUser currentUser, IDateTimeProvider dateTimeProvider) : IMiddleware
 {
-    private readonly ICurrentUser _currentUser;
-    private readonly IDateTimeProvider _dateTimeProvider;
-
-    public RequestLoggingMiddleware(ICurrentUser currentUser, IDateTimeProvider dateTimeProvider)
-    {
-        _currentUser = currentUser;
-        _dateTimeProvider = dateTimeProvider;
-    }
+    private readonly ICurrentUser _currentUser = currentUser;
+    private readonly IDateTimeProvider _dateTimeProvider = dateTimeProvider;
 
     public async Task InvokeAsync(HttpContext httpContext, RequestDelegate next)
     {
-        LogContext.PushProperty("RequestTimeUTC", _dateTimeProvider.Now);
-        string requestBody = string.Empty;
-        if (httpContext.Request.Path.ToString().Contains("tokens"))
+        using (LogContext.PushProperty("CorrelationId", httpContext.TraceIdentifier))
+        using (LogContext.PushProperty("RequestTimeUtc", _dateTimeProvider.Now))
+        using (LogContext.PushProperty("UserEmail", _currentUser.GetUserEmail() is string userEmail ? userEmail : "Anonymous"))
+        using (LogContext.PushProperty("UserId", _currentUser.GetUserId()))
         {
-            requestBody = "[Redacted] Contains Sensitive Information.";
+            await next(httpContext);
         }
-        else
-        {
-            var request = httpContext.Request;
-
-            if (!string.IsNullOrEmpty(request.ContentType)
-                && request.ContentType.StartsWith("application/json"))
-            {
-                request.EnableBuffering();
-                using var reader = new StreamReader(request.Body, Encoding.UTF8, true, 4096, true);
-                requestBody = await reader.ReadToEndAsync();
-
-                // rewind for next middleware.
-                request.Body.Position = 0;
-            }
-        }
-
-        string email = _currentUser.GetUserEmail() is string userEmail ? userEmail : "Anonymous";
-        var userId = _currentUser.GetUserId();
-
-        if (userId != Guid.Empty)
-            LogContext.PushProperty("UserId", userId);
-
-        LogContext.PushProperty("UserEmail", email);
-
-        LogContext.PushProperty("RequestBody", requestBody);
-        Log.ForContext("RequestHeaders", httpContext.Request.Headers.ToDictionary(h => h.Key, h => h.Value.ToString()), destructureObjects: true)
-           .ForContext("RequestBody", requestBody)
-           .Information("HTTP {RequestMethod} Request sent to {RequestPath}", httpContext.Request.Method, httpContext.Request.Path, string.IsNullOrEmpty(email) ? "Anonymous" : email);
-        await next(httpContext);
     }
 }
