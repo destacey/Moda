35af48eb10bda9739d7d31b6740e4ece8758560d
Dannie Stacey <dstacey@outlook.com>
2024-03-02 13:30:20 -0600

    Fix the issue where setting a value as false is stored as undefined.

--- Moda.Web/src/moda.web.reactclient/src/app/hooks/use-local-storage-state.ts
+++ Moda.Web/src/moda.web.reactclient/src/app/hooks/use-local-storage-state.ts
@@ -2,18 +2,20 @@ import { useEffect, useState } from 'react'
 
 export const useLocalStorageState = <T = any>(
   key: string,
-  defaultValue: T
+  defaultValue: T,
 ): [T, (value: T) => void] => {
   const [value, setValue] = useState<T>(() => {
     // If window is undefined, we are on the server and localStorage is not available
     if (typeof window !== 'undefined') {
       const storedValue = window.localStorage.getItem(key)
-      return storedValue && storedValue !== 'undefined' ? JSON.parse(storedValue) : defaultValue
+      return storedValue && storedValue !== 'undefined'
+        ? JSON.parse(storedValue)
+        : defaultValue
     }
   })
 
   useEffect(() => {
-    window.localStorage.setItem(key, value ? JSON.stringify(value) : undefined)
+    window.localStorage.setItem(key, JSON.stringify(value))
   }, [key, value])
 
   return [value, setValue]
