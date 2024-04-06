1b9e0288b49b1a7d8fab2acbc1f3112671e3a625
Dannie Stacey <dstacey@outlook.com>
2024-04-06 10:37:11 -0500

    Added content and a layout for the workspaces landing page.

--- Moda.Web/src/moda.web.reactclient/src/app/work/workspaces/page.tsx
+++ Moda.Web/src/moda.web.reactclient/src/app/work/workspaces/page.tsx
@@ -2,13 +2,77 @@
 
 import PageTitle from '@/src/app/components/common/page-title'
 import { authorizePage } from '../../components/hoc'
-import { useDocumentTitle } from '../../hooks'
+import { useAppDispatch, useAppSelector, useDocumentTitle } from '../../hooks'
+import { useGetWorkspacesQuery } from '@/src/store/features/work-management/workspace-api'
+import { useEffect, useMemo, useState } from 'react'
+import Segmented, { SegmentedLabeledOption } from 'antd/es/segmented'
+import { AppstoreOutlined, MenuOutlined } from '@ant-design/icons'
+import WorkspacesCardGrid from './workspaces-card-grid'
+import WorkspacesGrid from './workspaces-grid'
 
-const WorkspacesPage = () => {
+enum Views {
+  Cards,
+  List,
+}
+
+const viewSelectorOptions: SegmentedLabeledOption[] = [
+  {
+    value: Views.Cards,
+    icon: <AppstoreOutlined alt="Cards" title="Cards" />,
+  },
+  {
+    value: Views.List,
+    icon: <MenuOutlined alt="List" title="List" />,
+  },
+]
+
+const WorkspacesPage: React.FC = () => {
   useDocumentTitle('Workspaces')
+  const [currentView, setCurrentView] = useState<string | number>(Views.Cards)
+
+  // TODO: add the ability to filter by active/inactive workspaces on both views
+  //const { includeInactive } = useAppSelector((state) => state.workspace)
+
+  const {
+    data: workspaceData,
+    isLoading,
+    error,
+    refetch,
+  } = useGetWorkspacesQuery(true)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    error && console.error(error)
+  }, [error])
+
+  const viewSelector = useMemo(
+    () => (
+      <Segmented
+        options={viewSelectorOptions}
+        value={currentView}
+        onChange={setCurrentView}
+      />
+    ),
+    [currentView],
+  )
+
   return (
     <>
       <PageTitle title="Workspaces" />
+      {currentView === Views.Cards ? (
+        <WorkspacesCardGrid
+          workspaces={workspaceData}
+          viewSelector={viewSelector}
+          isLoading={isLoading}
+        />
+      ) : (
+        <WorkspacesGrid
+          workspaces={workspaceData}
+          viewSelector={viewSelector}
+          isLoading={isLoading}
+          refetch={refetch}
+        />
+      )}
     </>
   )
 }
---
+++ Moda.Web/src/moda.web.reactclient/src/app/work/workspaces/workspaces-grid.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { WorkspaceListDto } from '@/src/services/moda-api'
+import { ColDef } from 'ag-grid-community'
+import { Card, Flex, List } from 'antd'
+import { ReactElement, useCallback, useMemo } from 'react'
+import { ModaGrid } from '../../components/common'
+
+export interface WorkspacesGridProps {
+  workspaces: WorkspaceListDto[]
+  viewSelector: ReactElement
+  isLoading: boolean
+  refetch: () => void
+}
+
+const WorkspacesGrid = (props: WorkspacesGridProps) => {
+  const { refetch } = props
+
+  const columnDefs = useMemo<ColDef<WorkspaceListDto>[]>(
+    () => [
+      { field: 'key', width: 90 },
+      { field: 'name' },
+      { field: 'description', width: 300 },
+      { field: 'ownership.name', headerName: 'Ownership' },
+      { field: 'isActive' },
+    ],
+    [],
+  )
+
+  const refresh = useCallback(async () => {
+    refetch()
+  }, [refetch])
+
+  return (
+    <>
+      <ModaGrid
+        columnDefs={columnDefs}
+        rowData={props.workspaces}
+        loadData={refresh}
+        isDataLoading={props.isLoading}
+        toolbarActions={props.viewSelector}
+      />
+    </>
+  )
+}
+
+export default WorkspacesGrid
