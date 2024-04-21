d88cfe564d31d500be8b18754b5458309ab98158
Dannie Stacey <dstacey@outlook.com>
2024-04-21 15:41:28 -0500

    Added Work Item API endpoints and queries for get list and get details actions.

---
+++ Moda.Services/Moda.Work/src/Moda.Work.Application/WorkItems/Dtos/WorkItemListDto.cs
@@ -0,0 +1,23 @@
+﻿using Moda.Common.Application.Employees.Dtos;
+using Moda.Work.Application.Workspaces.Dtos;
+
+namespace Moda.Work.Application.WorkItems.Dtos;
+public sealed record WorkItemListDto : IMapFrom<WorkItem>
+{
+    public Guid Id { get; set; }
+    public required string Key { get; set; }
+    public required string Title { get; set; }
+    public required WorkspaceNavigationDto Workspace { get; set; }
+    public required string Type { get; set; }
+    public required string Status { get; set; }
+    public EmployeeNavigationDto? AssignedTo { get; set; }
+
+    public void ConfigureMapping(TypeAdapterConfig config)
+    {
+        config.NewConfig<WorkItem, WorkItemListDto>()
+            .Map(dest => dest.Key, src => src.Key.ToString())
+            .Map(dest => dest.Type, src => src.Type.Name)
+            .Map(dest => dest.Status, src => src.Status.Name)
+            .Map(dest => dest.AssignedTo, src => src.AssignedTo == null ? null : EmployeeNavigationDto.From(src.AssignedTo));
+    }
+}
---
+++ Moda.Services/Moda.Work/src/Moda.Work.Application/WorkItems/Queries/GetWorkItemsQuery.cs
@@ -0,0 +1,49 @@
+﻿using Moda.Common.Models;
+using Moda.Work.Application.WorkItems.Dtos;
+
+namespace Moda.Work.Application.WorkItems.Queries;
+public sealed record GetWorkItemsQuery : IQuery<Result<IReadOnlyCollection<WorkItemListDto>>>
+{
+    public GetWorkItemsQuery(Guid workspaceId)
+    {
+        Id = Guard.Against.NullOrEmpty(workspaceId);
+    }
+
+    public GetWorkItemsQuery(WorkspaceKey workspaceKey)
+    {
+        Key = Guard.Against.Default(workspaceKey);
+    }
+
+    public Guid? Id { get; }
+    public WorkspaceKey? Key { get; }
+}
+
+internal sealed class GetWorkItemsQueryHandler(IWorkDbContext workDbContext, ILogger<GetWorkItemsQueryHandler> logger) : IQueryHandler<GetWorkItemsQuery, Result<IReadOnlyCollection<WorkItemListDto>>>
+{
+    private const string AppRequestName = nameof(GetWorkItemsQuery);
+
+    private readonly IWorkDbContext _workDbContext = workDbContext;
+    private readonly ILogger<GetWorkItemsQueryHandler> _logger = logger;
+
+    public async Task<Result<IReadOnlyCollection<WorkItemListDto>>> Handle(GetWorkItemsQuery request, CancellationToken cancellationToken)
+    {
+        var query = _workDbContext.WorkItems
+            .AsQueryable();
+
+        if (request.Id.HasValue)
+        {
+            query = query.Where(e => e.WorkspaceId == request.Id);
+        }
+        else if (request.Key is not null)
+        {
+            query = query.Where(e => e.Workspace.Key == request.Key);
+        }
+        else
+        {
+            _logger.LogError("{AppRequestName}: No workspace id or key provided. {@Request}", AppRequestName, request);
+            return Result.Failure<IReadOnlyCollection<WorkItemListDto>>("No valid workspace id or key provided.");
+        }
+
+        return await query.ProjectToType<WorkItemListDto>().ToListAsync(cancellationToken);
+    }
+}
