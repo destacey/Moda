c135e0b0dc300358ee47139396ee3012eeb4776e
Dannie Stacey <dstacey@outlook.com>
2024-04-27 10:05:19 -0500

    Added the new work item search endpoint to the store.

--- Moda.Web/src/moda.web.reactclient/src/store/features/query-tags.ts
+++ Moda.Web/src/moda.web.reactclient/src/store/features/query-tags.ts
@@ -1,6 +1,7 @@
 export enum QueryTags {
   // WORK MANAGEMENT
   WorkItem = 'Work.WorkItem',
+  WorkItemSearch = 'Work.WorkItem.Search',
   WorkProcess = 'Work.WorkProcess',
   Workspace = 'Work.Workspace',
 }
