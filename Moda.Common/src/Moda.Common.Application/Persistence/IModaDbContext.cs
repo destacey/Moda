70102c7a41c04bcc212ac4a408eec0aed76c7de6
Dannie Stacey <dstacey@outlook.com>
2024-11-30 09:48:16 -0600

    Added the EF DatabaseFacade to the IModaDbContext interface.  This will simplify the process for writing sql queries for non-entity types.

--- Moda.Common/src/Moda.Common.Application/Persistence/IModaDbContext.cs
+++ Moda.Common/src/Moda.Common.Application/Persistence/IModaDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Moda.Common.Domain.Employees;
 
 namespace Moda.Common.Application.Persistence;
 public interface IModaDbContext
 {
+    // this dependency is bigger than needed, but most of the extensions methods are leveraging it. 
+    DatabaseFacade Database { get; }
     ChangeTracker ChangeTracker { get; }
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
