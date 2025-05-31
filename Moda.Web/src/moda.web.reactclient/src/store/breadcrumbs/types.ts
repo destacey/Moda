43807a69d799ede2e2628ce1bb8c10ccc7483b0b
Dannie Stacey <dstacey@outlook.com>
2025-05-31 14:14:53 -0500

    Updated types.

--- Moda.Web/src/moda.web.reactclient/src/store/breadcrumbs/types.ts
+++ Moda.Web/src/moda.web.reactclient/src/store/breadcrumbs/types.ts
@@ -1,12 +1,17 @@
-
 export interface BreadcrumbState {
-  items: BreadcrumbItem[];
-  isVisible: boolean;
-  forPath: string;
+  items: BreadcrumbItem[]
+  isVisible: boolean
+  forPath: string
 }
 
 export interface BreadcrumbItem {
-  title: string;
-  href?: string;
-  path?: string;
+  title: string
+  href?: string
+  path?: string
+  key?: string | number
+  className?: string
+  dropdownProps?: any
+  onClick?: (e?: React.MouseEvent<HTMLAnchorElement>) => void
+  // Add index signature for data attributes
+  [key: `data-${string}`]: any
 }
