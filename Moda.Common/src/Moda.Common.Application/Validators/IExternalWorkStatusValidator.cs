b8bec9d72b83041e6faf2ff3027a923c0a6de450
Dannie Stacey <dstacey@outlook.com>
2025-12-07 12:35:10 -0800

    Moved validator.

---
+++ Moda.Common/src/Moda.Common.Application/Validators/IExternalWorkStatusValidator.cs
@@ -0,0 +1,12 @@
+﻿using Moda.Common.Application.Interfaces.ExternalWork;
+
+namespace Moda.Common.Application.Validators;
+public sealed class IExternalWorkStatusValidator : CustomValidator<IExternalWorkStatus>
+{
+    public IExternalWorkStatusValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty()
+            .MaximumLength(64);
+    }
+}
