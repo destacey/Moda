3567ad69163c7edc7a2924bd82f191db6c7b8cd1
Dannie Stacey <dstacey@outlook.com>
2025-12-13 11:09:27 -0800

    Added a new LifecyclePhase enum to use on enums that represent workflows.

---
+++ Moda.Common/src/Moda.Common.Application/Dtos/LifecycleNavigationDto.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moda.Common.Application.Dtos;
+public record LifecycleNavigationDto
+{
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public required string Name { get; set; }
+
+    [Required]
+    public required string LifecyclePhase { get; set; }
+
+    public static LifecycleNavigationDto FromEnum<T>(T value) where T : struct, Enum
+    {
+        return new()
+        {
+            Id = (int)(object)value,
+            Name = value.GetDisplayName(),
+            LifecyclePhase = value.GetDisplayGroupName() ?? "Unknown"
+        };
+    }
+
+    public static LifecycleNavigationDto FromEnum<T>(int value) where T : struct, Enum
+    {
+        return new()
+        {
+            Id = value,
+            Name = ((T)(object)value).GetDisplayName(),
+            LifecyclePhase = ((T)(object)value).GetDisplayGroupName() ?? "Unknown"
+        };
+    }
+}
---
+++ Moda.Common/src/Moda.Common.Domain/Enums/LifecyclePhase.cs
@@ -0,0 +1,13 @@
+﻿namespace Moda.Common.Domain.Enums;
+
+/// <summary>
+/// Represents the high-level position of an item in its lifecycle.
+/// This is descriptive metadata only and does not imply success, failure,
+/// or the reason the work ended.
+/// </summary>
+public enum LifecyclePhase
+{
+    NotStarted,
+    Active,
+    Done
+}
