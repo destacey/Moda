b3cc020f2750f6f99664b1b8304314b3ef97936d
Dannie Stacey <dstacey@outlook.com>
2026-02-07 17:15:42 -0800

    Added an extra prop to the PageTitle component.

--- Moda.Web/src/moda.web.reactclient/src/components/common/page-title.tsx
+++ Moda.Web/src/moda.web.reactclient/src/components/common/page-title.tsx
@@ -1,39 +1,50 @@
-import { Col, Row, Space, Typography } from 'antd'
+import { Col, Flex, Row, Space, Typography } from 'antd'
+import { ReactNode } from 'react'
 
 const { Title, Text } = Typography
 
 export interface PageTitleProps {
-  title: string | React.ReactNode
+  title: string | ReactNode
   subtitle?: string
-  tags?: React.ReactNode | null
-  actions?: React.ReactNode | null
+  tags?: ReactNode | null
+  actions?: ReactNode | null
+  extra?: ReactNode | null
 }
 
 // TODO: align actions to the right/end when not the xs or sm breakpoint
-const PageTitle = ({ title, subtitle, tags, actions }: PageTitleProps) => {
+const PageTitle = ({
+  title,
+  subtitle,
+  tags,
+  actions,
+  extra,
+}: PageTitleProps) => {
   const titleMdSize = actions ? 16 : 24
   return (
     <>
-      <Row align={'middle'} style={{ marginBottom: '12px' }}>
-        <Col xs={24} sm={24} md={titleMdSize}>
-          <Space>
-            <div>
-              <Title level={2} style={{ margin: '0px', fontWeight: '400' }}>
-                {title}
-              </Title>
-              {subtitle && <Text>{subtitle}</Text>}
-            </div>
-            {tags && <div>{tags}</div>}
-          </Space>
-        </Col>
-        {actions && (
-          <Col xs={24} sm={24} md={8}>
-            <Space style={{ display: 'flex', justifyContent: 'flex-end' }}>
-              {actions}
+      <Flex vertical gap={8} style={{ marginBottom: 12 }}>
+        <Row align={'middle'}>
+          <Col xs={24} sm={24} md={titleMdSize}>
+            <Space>
+              <div>
+                <Title level={2} style={{ margin: '0px', fontWeight: '400' }}>
+                  {title}
+                </Title>
+                {subtitle && <Text>{subtitle}</Text>}
+              </div>
+              {tags && <div>{tags}</div>}
             </Space>
           </Col>
-        )}
-      </Row>
+          {actions && (
+            <Col xs={24} sm={24} md={8}>
+              <Space style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                {actions}
+              </Space>
+            </Col>
+          )}
+        </Row>
+        {extra && <Row>{extra}</Row>}
+      </Flex>
     </>
   )
 }
