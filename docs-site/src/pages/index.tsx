8b46b40e42da47314a3be1f9f17fd22c8b06687d
Dannie Stacey <dstacey@outlook.com>
2026-03-25 21:03:55 -0700

    Fix redirect to useBaseUrl for documentation path in Home component

--- docs-site/src/pages/index.tsx
+++ docs-site/src/pages/index.tsx
@@ -1,5 +1,8 @@
-import {Redirect} from '@docusaurus/router';
+import { Redirect } from "@docusaurus/router";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import { JSX } from "react";
 
 export default function Home(): JSX.Element {
-  return <Redirect to="/docs" />;
+  return <Redirect to={useBaseUrl("/docs")} />;
 }
+
