52927392bc958b5cdcadb402dfd75a1c670fd01d
Dannie Stacey <dstacey@outlook.com>
2026-04-26 09:31:39 -0700

    Enhance WaydTooltip component to support help cursor functionality

--- Wayd.Web/src/wayd.web.reactclient/src/components/common/wayd-tooltip.tsx
+++ Wayd.Web/src/wayd.web.reactclient/src/components/common/wayd-tooltip.tsx
@@ -1,9 +1,50 @@
 'use client'
 
 import { Tooltip, TooltipProps } from 'antd'
+import {
+  cloneElement,
+  CSSProperties,
+  isValidElement,
+  ReactElement,
+  ReactNode,
+} from 'react'
 
-const WaydTooltip = (props: TooltipProps) => (
-  <Tooltip mouseEnterDelay={0.5} {...props} />
-)
+interface WaydTooltipProps extends TooltipProps {
+  helpCursor?: boolean
+}
+
+const WaydTooltip = (props: WaydTooltipProps) => {
+  const { helpCursor = false, children, ...tooltipProps } = props
+
+  if (!helpCursor) {
+    return (
+      <Tooltip mouseEnterDelay={0.5} {...tooltipProps}>
+        {children}
+      </Tooltip>
+    )
+  }
+
+  const cursorStyle = { cursor: 'help' as const }
+
+  const childWithCursor = isValidElement(children)
+    ? (() => {
+        const child = children as ReactElement<{ style?: CSSProperties }>
+        return cloneElement(child, {
+          style: {
+            ...(child.props.style ?? {}),
+            ...cursorStyle,
+          },
+        })
+      })()
+    : // Wrap non-element children to apply cursor without requiring callers
+      // to add extra markup.
+      <span style={cursorStyle}>{children as ReactNode}</span>
+
+  return (
+    <Tooltip mouseEnterDelay={0.5} {...tooltipProps}>
+      {childWithCursor}
+    </Tooltip>
+  )
+}
 
 export default WaydTooltip
